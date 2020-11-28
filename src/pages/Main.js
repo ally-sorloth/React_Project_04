3a399e2c26d982641e81e02579d8c4e91fc361ba
ally-sorloth <allysorloth@gmail.com>
2020-11-28 21:53:05 +0300

    add user detail api

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,7 @@
   
 import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Container } from "@material-ui/core";
+import { Container, Grid, capitalize } from "@material-ui/core";
 import axios from "axios";
 import MediaCard from "../components/MediaCard";
 
@@ -36,17 +36,23 @@ function Main() {
   }, []);
 
   return (
-    <Container className={mainStyles.wrapper} maxWidth="sm">
-      {userList?.map((user) => {
-        return (
-          <MediaCard
-            key={user?.id}
-            userImage={user?.picture}
-            userName={`${user?.title} ${user?.firstName} ${user?.lastName}`}
-            userEmail={user?.email}
-          />
-        );
-      })}
+    <Container className={mainStyles.wrapper}>
+      <Grid container spacing={1}>
+        {userList?.map((user) => {
+          return (
+            <Grid item sm={4} xs={6} key={user?.id}>
+              <MediaCard
+                id={user.id}
+                userImage={user?.picture}
+                userName={`${capitalize(user?.title)} ${user?.firstName} ${
+                  user?.lastName
+                }`}
+                userEmail={user?.email}
+              />
+            </Grid>
+          );
+        })}
+      </Grid>
     </Container>
   );
 }
--- src/router/Router.js
+++ src/router/Router.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Main from "../pages/Main";
 import Signup from "../pages/Signup";
 import Signin from "../pages/Signin";
+import UserDetail from "../pages/UserDetail";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
@@ -12,9 +13,9 @@ function AppRouter() {
       <Navbar />
       <Switch>
         <Route exact path="/register" component={Signup} />
-        <Route  exact path="/login" component={Signin} />
+        <Route exact path="/login" component={Signin} />
+        <Route exact path="/user/:id" component={UserDetail} />
         <Route path="/" component={Main} />
-        
       </Switch>
       <Footer />
     </Router>
